755ea07f576609097cb2a6e3b8cd7aefc112e823
= <help.studentgrade@gmail.com>
2022-08-28 17:02:01 +0600

    added use state hook with Array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import UseState from '../src/components/UseState/UseState';
 // import PrevState from '../src/components/UseState/PrevState';
-import UseStateWithObject from '../src/components/UseState/UseStateWithObject';
+// import UseStateWithObject from '../src/components/UseState/UseStateWithObject';
+import UseStateWithArray from '../src/components/UseState/UseStateWithArray';
 
 
 function App() {
     return (
         <div className="App">
-            <UseStateWithObject/>
+            {/*<UseStateWithObject/>*/}
+            <UseStateWithArray/>}
         </div>
     );
 }
