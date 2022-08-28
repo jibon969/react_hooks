06694b5f31cba1685e20813834b23877bfc304e2
= <help.studentgrade@gmail.com>
2022-08-28 17:10:49 +0600

    added use effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 // import UseState from '../src/components/UseState/UseState';
 // import PrevState from '../src/components/UseState/PrevState';
 // import UseStateWithObject from '../src/components/UseState/UseStateWithObject';
-import UseStateWithArray from '../src/components/UseState/UseStateWithArray';
+// import UseStateWithArray from '../src/components/UseState/UseStateWithArray';
+
+import UseEffectAfterRender from '../src/components/UseEffect/UseEffectAfterRender';
 
 
 function App() {
     return (
         <div className="App">
             {/*<UseStateWithObject/>*/}
-            <UseStateWithArray/>}
+            {/*<UseStateWithArray/>}*/}
+            <UseEffectAfterRender/>
         </div>
     );
 }
