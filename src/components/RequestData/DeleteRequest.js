ae9cc23d77d21b0115bb249d8af37357af756292
= <help.studentgrade@gmail.com>
2022-08-31 22:58:45 +0600

    added request delete component

---
+++ src/components/RequestData/DeleteRequest.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+const DeleteRequest = () => {
+
+    return (
+        <>
+            <div className="contentSection">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-6 mt-5">
+                            <div className="card">
+                                <div className="card-body">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default DeleteRequest;
+
+
