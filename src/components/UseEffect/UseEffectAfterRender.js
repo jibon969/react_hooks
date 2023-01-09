b4a39b5ac1278f3aa3f0226d62787163069ec580
jayed969 <jayed.swe@gmail.com>
2023-01-09 22:43:05 +0600

    remove console log

--- src/components/UseEffect/UseEffectAfterRender.js
+++ src/components/UseEffect/UseEffectAfterRender.js
@@ -1,14 +1,13 @@
 import React, {useState, useEffect} from 'react'
 
 const UseEffectAfterRender = () => {
-
     const [count, setCount] = useState(0);
 
     useEffect(()=>{
         document.title = `Count - ${count}`
     });
 
-    console.log("Count :", count);
+    // console.log("Count :", count);
 
     return (
         <div>
